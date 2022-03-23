4099844cd0297570aacb452ec8d91cc2758b7915
k12345ji <43379220+k12345ji@users.noreply.github.com>
2022-03-23 16:56:41 +0800

    Update actionmap_common.h

--- keyboard/Omega45/e/actionmap_common.h
+++ keyboard/Omega45/e/actionmap_common.h
@@ -34,26 +34,34 @@ extern const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS];
  * 第二组为按键实际矩阵
  */
 #define ACTIONMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-	K30, K31, K32, K33,      K35,           K38, K39, K3A, K3B  \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32, K33, K34,           K37, K38, K39, K3A, K3B  \
 ) { \
-	{ AC_##K00,    AC_##K01,    AC_##K02,    AC_##K03,    AC_##K04,    AC_##K05,    AC_##K06,    AC_##K07,    AC_##K08,    AC_##K09,    AC_##K0A,    AC_##K0B,   AC_NO }, \
-	{ AC_##K10,    AC_##K11,    AC_##K12,    AC_##K13,    AC_##K14,    AC_##K15,    AC_##K16,    AC_##K17,    AC_##K18,    AC_##K19,    AC_##K1A,    AC_##K1B,   AC_NO }, \
-	{ AC_##K20,    AC_##K21,    AC_##K22,    AC_##K23,    AC_##K24,    AC_##K25,    AC_##K26,    AC_##K27,    AC_##K28,    AC_##K29,    AC_##K2A,    AC_##K2B,   AC_NO }, \
-	{ AC_##K30,    AC_##K31,    AC_##K32,    AC_##K33,    AC_NO,       AC_##K35,    AC_NO,       AC_NO,       AC_##K38,    AC_##K39,    AC_##K3A,    AC_##K3B,   AC_NO }  \
+	{ AC_##K05,    AC_##K04,    AC_##K03,    AC_##K02,    AC_##K01,    AC_##K00 }, \
+	{ AC_##K15,    AC_##K14,    AC_##K13,    AC_##K12,    AC_##K11,    AC_##K10 }, \
+	{ AC_##K25,    AC_##K24,    AC_##K23,    AC_##K22,    AC_##K21,    AC_##K20 }, \
+	{ AC_NO   ,    AC_##K34,    AC_##K33,    AC_##K32,    AC_##K31,    AC_##K30 }, \
+        { AC_##K06,    AC_##K07,    AC_##K08,    AC_##K09,    AC_##K0A,    AC_##K0B }, \
+        { AC_##K16,    AC_##K17,    AC_##K18,    AC_##K19,    AC_##K1A,    AC_##K1B }, \
+        { AC_##K26,    AC_##K27,    AC_##K28,    AC_##K29,    AC_##K2A,    AC_##K2B }, \
+	{ AC_NO   ,    AC_##K37,    AC_##K38,    AC_##K39,    AC_##K3A,    AC_##K3B }  \
 }
 
 #define ACTIONMAP_ROT( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-	K30, K31, K32, K33,      K35,           K38, K39, K3A, K3B, \
-	R0P, R0N, K3C \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32, K33, K34,           K37, K38, K39, K3A, K3B, \
+    R0P, R0N \
 ) { \
-	{ AC_##K00,    AC_##K01,    AC_##K02,    AC_##K03,    AC_##K04,    AC_##K05,    AC_##K06,    AC_##K07,    AC_##K08,    AC_##K09,    AC_##K0A,    AC_##K0B,   AC_NO }, \
-	{ AC_##K10,    AC_##K11,    AC_##K12,    AC_##K13,    AC_##K14,    AC_##K15,    AC_##K16,    AC_##K17,    AC_##K18,    AC_##K19,    AC_##K1A,    AC_##K1B,   AC_NO }, \
-	{ AC_##K20,    AC_##K21,    AC_##K22,    AC_##K23,    AC_##K24,    AC_##K25,    AC_##K26,    AC_##K27,    AC_##K28,    AC_##K29,    AC_##K2A,    AC_##K2B,   AC_NO }, \
-	{ AC_##K30,    AC_##K31,    AC_##K32,    AC_##K33,    AC_##R0P,    AC_##K35,    AC_##R0N,    AC_NO,       AC_##K38,    AC_##K39,    AC_##K3A,    AC_##K3B,   AC_##K3C }  \
+	{ AC_##K05,    AC_##K04,    AC_##K03,    AC_##K02,    AC_##K01,    AC_##K00 }, \
+	{ AC_##K15,    AC_##K14,    AC_##K13,    AC_##K12,    AC_##K11,    AC_##K10 }, \
+	{ AC_##K25,    AC_##K24,    AC_##K23,    AC_##K22,    AC_##K21,    AC_##K20 }, \
+	{ AC_##R0P,    AC_##K34,    AC_##K33,    AC_##K32,    AC_##K31,    AC_##K30 }, \
+        { AC_##K06,    AC_##K07,    AC_##K08,    AC_##K09,    AC_##K0A,    AC_##K0B }, \
+        { AC_##K16,    AC_##K17,    AC_##K18,    AC_##K19,    AC_##K1A,    AC_##K1B }, \
+        { AC_##K26,    AC_##K27,    AC_##K28,    AC_##K29,    AC_##K2A,    AC_##K2B }, \
+	{ AC_##R0N,    AC_##K37,    AC_##K38,    AC_##K39,    AC_##K3A,    AC_##K3B }  \
 }
