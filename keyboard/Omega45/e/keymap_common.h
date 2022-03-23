b74fab9e079d06a8f62ad564cdbc8742838d6cb4
k12345ji <43379220+k12345ji@users.noreply.github.com>
2022-03-23 16:34:35 +0800

    Update keymap_common.h

--- keyboard/Omega45/e/keymap_common.h
+++ keyboard/Omega45/e/keymap_common.h
@@ -35,30 +35,34 @@ extern const action_t fn_actions[];
  * 第二组为按键实际矩阵
  */
 #define KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-	K40, K41, K42, K43,      K45,      K47, K48, K49, K4A, K4B  \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32, K33, K34,           K37, K38, K39, K3A, K3B  \
 ) { \
-	{ K00,    K01,    K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K0A,    K0B,    KC_NO }, \
-	{ K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K1A,    K1B,    KC_NO }, \
-	{ K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K2A,    K2B,    KC_NO }, \
-	{ K30,    K31,    K32,    K33,    K34,    K35,    K36,    K37,    K38,    K39,    K3A,    K3B,    KC_NO }, \
-	{ K40,    K41,    K42,    K43,    KC_NO,  K45,    KC_NO,  K47,    K48,    K49,    K4A,    K4B,    KC_NO }  \
+    { K05, K04, K03, K02, K01, K00 }, \
+    { K15, K14, K13, K12, K11, K10 }, \
+    { K25, K24, K23, K22, K21, K20 }, \
+    { KC_NO, K34, K33, K32, K31, K30 }, \
+    { K06, K07, K08, K09, K0A, K0B }, \
+    { K16, K17, K18, K19, K1A, K1B }, \
+    { K26, K27, K28, K29, K2A, K2B }, \
+    { KC_NO, K37, K38, K39, K3A, K3B }  \
 }
 
 #define KEYMAP_ROT( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-	K40, K41, K42, K43,      K45,      K47, K48, K49, K4A, K4B, \
-	R0P, R0N, K4C \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32, K33, K34,           K37, K38, K39, K3A, K3B  \
+    R0P, R0N \
 ) { \
-	{ K00,    K01,    K02,    K03,    K04,    K05,    K06,    K07,    K08,    K09,    K0A,    K0B,    KC_NO }, \
-	{ K10,    K11,    K12,    K13,    K14,    K15,    K16,    K17,    K18,    K19,    K1A,    K1B,    KC_NO }, \
-	{ K20,    K21,    K22,    K23,    K24,    K25,    K26,    K27,    K28,    K29,    K2A,    K2B,    KC_NO }, \
-	{ K30,    K31,    K32,    K33,    K34,    K35,    K36,    K37,    K38,    K39,    K3A,    K3B,    KC_NO }, \
-	{ K40,    K41,    K42,    K43,    R0P,    K45,    R0N,    K47,    K48,    K49,    K4A,    K4B,    K4C }  \
+    { K05, K04, K03, K02, K01, K00 }, \
+    { K15, K14, K13, K12, K11, K10 }, \
+    { K25, K24, K23, K22, K21, K20 }, \
+    { R0P, K34, K33, K32, K31, K30 }, \
+    { K06, K07, K08, K09, K0A, K0B }, \
+    { K16, K17, K18, K19, K1A, K1B }, \
+    { K26, K27, K28, K29, K2A, K2B }, \
+    { R0N, K37, K38, K39, K3A, K3B }  \
 }
