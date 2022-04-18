ade5ab4008dd3404fb0a9ee93e57aef8702e615d
k12345ji <43379220+k12345ji@users.noreply.github.com>
2022-04-18 22:41:39 +0800

    Update config.h

--- keyboard/Omega45/e/config.h
+++ keyboard/Omega45/e/config.h
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_FUNCTION
 
 // LED 配置
-// #define LED_CAPS 11  //caps led ：5 ； led1、2、3、4：19、20、27、26
+#define LED_CAPS 16  //caps led ：5 ； led1、2、3、4：19、20、27、26
 
 // ws2812 RGB 配置
 #define RGB_DI_PIN 6
@@ -98,8 +98,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // USB UART 传输配置
 #define HAS_USB // 启用与CH554的通信支持
-#define UART_RXD 12 // UART_RX口IO 17
-#define UART_TXD 13 // UART_TX口IO 18
+#define UART_RXD 11 // UART_RX口IO 17
+#define UART_TXD 12 // UART_TX口IO 18
 //#define UART_DET 19 // UART 检测引脚，若此脚被拉低，则说明USB正在工作。若不配置则使用RX口作为检测引脚
 #define UART_BAUDRATE NRF_UART_BAUDRATE_115200 // 通信波特率，请不要修改
 
@@ -126,7 +126,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
 static const uint8_t row_pin_array[MATRIX_ROWS] = {9, 6, 5, 4, 10, 8, 26, 25};
-static const uint8_t column_pin_array[MATRIX_COLS] = {15, 16, 14, 31, 30, 27, 29};
+static const uint8_t column_pin_array[MATRIX_COLS] = {14, 15, 13, 31, 30, 27, 29};
 
 #define ROW_IN // 键盘阵列的二极管方向是从COL->ROW
 
